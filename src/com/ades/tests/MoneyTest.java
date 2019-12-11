5b3fc709f10f78892ebbc5596acd7a2f0b696162
Arthur Deschaud <airthur.deschaud@gmail.com>
2019-12-11 15:39:44 +0100

    adding a test to Money class

--- src/com/ades/tests/MoneyTest.java
+++ src/com/ades/tests/MoneyTest.java
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.Test;
 import com.ades.moneybag.Money;
 
 public class MoneyTest {
-	
+
 	@Test
 	void testGetAmount() {
 		Money m = new Money(5,"EUR");
 		Assertions.assertEquals(5, m.amount());
 	}
-	
+
 	@Test
 	void testGetCurrency() {
 		Money m = new Money(5,"EUR");
 		Assertions.assertEquals("EUR", m.currency());
 	}
-	
+
 	@Test
 	void testAddMoneySameCurrency() {
 		Money m1 = new Money(5,"EUR");
@@ -34,7 +34,7 @@ public class MoneyTest {
 		m1.add(m2);
 		Assertions.assertEquals(5, m1.amount());
 	}
-	
+
 	@Test
 	void testAddNegativeMoney() {
 		Money m1 = new Money(5,"EUR");
@@ -42,26 +42,33 @@ public class MoneyTest {
 		m1.add(m2);
 		Assertions.assertEquals(3, m1.amount());
 	}
-	
+
 	@Test
 	void testAddWithAmountAndCurrency(){
 		Money m1 = new Money(5,"EUR");
 		m1.add(12,"EUR");
 		Assertions.assertEquals(17,m1.amount());
 	}
-	
+
 	@Test
 	void testAddWithAmountAndDifferentCurrency() {
 		Money m1 = new Money(5,"EUR");
 		m1.add(12,"USD");
 		Assertions.assertEquals(5,m1.amount());
 	}
-	
+
+	@Test
+	void testAddNegativeWithAmountAndCurrency(){
+		Money m1 = new Money(5,"EUR");
+		m1.add(-2,"EUR");
+		Assertions.assertEquals(3,m1.amount());
+	}
+
 	@Test
 	void testConstructor() {
 		Money m1 = new Money(5,"EUR");
 		Assertions.assertEquals(5, m1.amount());
 		Assertions.assertEquals("EUR",m1.currency());
 	}
-	
+
 }
